c144ed048fcfefb83d0faa1bb05020d72b226e82
Ashfaq Nisar <ashfaqnisar00@gmail.com>
2020-04-13 22:06:37 +0530

    Updated the images in the home page

--- components/DigitalMarketing/Banner.js
+++ components/DigitalMarketing/Banner.js
@@ -160,26 +160,24 @@ class Banner extends React.Component {
 
                     <div className="bg-gray shape-1"></div>
                     <div className="shape1"><img src={require('../../images/shape1.png')} alt="img"/></div>
-                    <div className="shape2"><img src={require('../../images/shape2.png')} alt="img"/></div>
                     <div className="shape3"><img src={require('../../images/shape3.png')} alt="img"/></div>
                     <div className="shape6"><img src={require('../../images/shape6.png')} alt="img"/></div>
                     <div className="shape8 rotateme"><img src={require('../../images/shape8.svg')} alt="shape"/></div>
                     <div className="shape9"><img src={require('../../images/shape9.svg')} alt="shape"/></div>
+                    <div className="shape10"><img src={require('../../images/shape10.svg')} alt="shape"/></div>
+
 
                     <div className="bubble-animate">
                         <div className="circle small square1"></div>
                         <div className="circle small square2"></div>
                         <div className="circle small square3"></div>
-                        <div className="circle small square4"></div>
                         <div className="circle small square5"></div>
                         <div className="circle medium square1"></div>
-                        <div className="circle medium square2"></div>
                         <div className="circle medium square3"></div>
                         <div className="circle medium square4"></div>
                         <div className="circle medium square5"></div>
                         <div className="circle large square1"></div>
                         <div className="circle large square2"></div>
-                        <div className="circle large square3"></div>
                         <div className="circle large square4"></div>
                     </div>
                 </div>

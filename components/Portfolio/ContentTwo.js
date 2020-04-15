0e07f4e179784ecbc6af8adc962caf7e2dd519d5
Ashfaq Nisar <ashfaqnisar00@gmail.com>
2020-04-15 15:16:25 +0530

    Removed the pagination

--- components/Portfolio/ContentTwo.js
+++ components/Portfolio/ContentTwo.js
@@ -241,24 +241,24 @@ class ContentTwo extends Component {
                             </div>
                         </div>
 
-                        <div className="col-lg-12 col-md-12">
-                            <div className="pagination-area">
-                                <nav aria-label="Page navigation example">
-                                    <ul className="pagination justify-content-center">
-                                    
-                                        <li className="page-item"><a className="page-link" href="#"><i className="icofont-double-left"></i></a></li>
-                                        
-                                        <li className="page-item active"><a className="page-link" href="#">1</a></li>
-                                        
-                                        <li className="page-item"><a className="page-link" href="#">2</a></li>
-                                        
-                                        <li className="page-item"><a className="page-link" href="#">3</a></li>
-                                        
-                                        <li className="page-item"><a className="page-link" href="#"><i className="icofont-double-right"></i></a></li>
-                                    </ul>
-                                </nav>
-                            </div>
-                        </div>
+                        {/*<div className="col-lg-12 col-md-12">*/}
+                        {/*    <div className="pagination-area">*/}
+                        {/*        <nav aria-label="Page navigation example">*/}
+                        {/*            <ul className="pagination justify-content-center">*/}
+                        {/*            */}
+                        {/*                <li className="page-item"><a className="page-link" href="#"><i className="icofont-double-left"></i></a></li>*/}
+                        {/*                */}
+                        {/*                <li className="page-item active"><a className="page-link" href="#">1</a></li>*/}
+                        {/*                */}
+                        {/*                <li className="page-item"><a className="page-link" href="#">2</a></li>*/}
+                        {/*                */}
+                        {/*                <li className="page-item"><a className="page-link" href="#">3</a></li>*/}
+                        {/*                */}
+                        {/*                <li className="page-item"><a className="page-link" href="#"><i className="icofont-double-right"></i></a></li>*/}
+                        {/*            </ul>*/}
+                        {/*        </nav>*/}
+                        {/*    </div>*/}
+                        {/*</div>*/}
                     </div>
                 </div>
             </section>

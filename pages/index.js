ab0aedf46ffe3e8136ad713eacf923511bf3bf68
Ashfaq Nisar <ashfaqnisar00@gmail.com>
2020-04-16 12:49:26 +0530

    Modified the projects section to have the correct behaviour

--- pages/index.js
+++ pages/index.js
@@ -2,16 +2,11 @@ import React from 'react';
 import Navbar from '../components/Layouts/Navbar';
 import Footer from '../components/Layouts/Footer';
 import Banner from '../components/DigitalMarketing/Banner';
-import Partner from '../components/DigitalMarketing/Partner';
-import Features from '../components/DigitalMarketing/Features';
 import Services from '../components/DigitalMarketing/Services';
 import Overview from '../components/DigitalMarketing/Overview';
-import Join from '../components/DigitalMarketing/Join';
 import AnalysisForm from '../components/DigitalMarketing/AnalysisForm';
-import Funfact from '../components/DigitalMarketing/Funfact';
 import Works from '../components/DigitalMarketing/Works';
 import Team from '../components/DigitalMarketing/Team';
-import Pricing from '../components/DigitalMarketing/Pricing';
 import Blog from '../components/DigitalMarketing/Blog';
 import Feedback from '../components/DigitalMarketing/Feedback';
 import GetStarted from '../components/DigitalMarketing/GetStarted';
@@ -21,19 +16,18 @@ class Index extends React.Component {
     render() {
         return (
             <React.Fragment>
-                <Navbar />
-                <Banner />
-                <Overview />
-                <Services />
-                <AnalysisForm />
-                <Funfact />
-                <Works />
-                <Team />
-                <Blog />
-                <Feedback />
-                <GetStarted />
-                <Platform />
-                <Footer />
+                <Navbar/>
+                <Banner/>
+                <Overview/>
+                <Services/>
+                {/*<Funfact />*/}
+                <Works/>
+                <AnalysisForm/>
+                <Blog/>
+                <Feedback/>
+                <GetStarted/>
+                <Platform/>
+                <Footer/>
             </React.Fragment>
         );
     }
